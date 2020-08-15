c8fc9d36321de01c5b40eb2058f82a28ddc1346f
sudhirb+1-at-887814685668 <sudhir.bisane@gmail.com>
2020-08-15 19:15:46 +0530

    hello

---
+++ filemanager-react/my-app/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import "./index.css";
+import flower from './flower.jpg'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/home.component";
+
+function App() {
+  return (<Router>
+    <div className="App" style={{marginTop:"57px", backgroundImage :`url(${flower})`,height: '600px', }}>
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        
+        <div className="container">
+            <Link className="navbar-brand">REMO FILE MANAGER</Link>
+        </div> 
+         
+        </nav>
+        
+        <div >
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path="/home" component={Home} />
+          </Switch>
+        </div>
+      
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ filemanager-react/my-app/src/components/home.component.js
@@ -0,0 +1,179 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './data.json';
+import { Link } from "react-router-dom";
+
+const indexData = data.index;
+
+class Folder2 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: indexData,
+            subFoldersData: [],
+            folderSelected: false,
+            previousClick: [],
+            folderPath: [],
+        }
+    }
+    writeLogToDB = () => {
+
+       var logPathArray=[];
+      logPathArray=this.state.folderPath;
+        
+       var log="root";
+       
+       for(var i=0;logPathArray.length>0;i++){
+          var temp=log+"/"+logPathArray[i];
+          log=temp
+
+       }
+       
+        debugger
+        const requestOptions = {
+
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ logname:log })
+        };
+        fetch('http://localhost:8081/saveLog', requestOptions)
+            .then(response => response.json())
+            .then( data =>alert("Added"));
+    }
+
+
+
+    handleClick = (e, s) => {
+        debugger;
+        console.log(s);
+
+        if (s.type === "folder") {
+            var tempFolderPath = []
+            var previousFolder = [];
+            previousFolder.push(...this.state.previousClick);
+            previousFolder.push(this.state.index);
+
+            tempFolderPath.push(...this.state.folderPath);
+            tempFolderPath.push(s.name);
+            this.setState({
+                index: s.files,
+                previousClick: previousFolder,
+                folderPath: tempFolderPath
+            })
+        }
+        else{
+            alert("No Data Available");
+        }
+    }
+    loodPriviousFolder = () => {
+        var tempFolderpath = [];
+        var previousFolder = this.state.previousClick;
+        var lastindex = previousFolder.length;
+
+
+        if (lastindex > 0) {
+            var lastDirectoryFolder = previousFolder[lastindex - 1]
+            previousFolder.splice(lastindex - 1, 1);
+            tempFolderpath = this.removeFolderFrpmPath();
+            this.setState({ index: lastDirectoryFolder, previousClick: previousFolder, folderPath: tempFolderpath });
+        }
+
+    }
+    removeFolderFrpmPath = () => {
+        var tempFolderPath = this.state.folderPath;
+        var lastindex = tempFolderPath.length;
+        tempFolderPath.splice(lastindex - 1, 1)
+        return tempFolderPath;
+    }
+
+    render() {
+
+        return (
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <div class="container">
+                                     
+                        <form onSubmit="return false" >
+                        <button type="button" className="btn btn-outline-light text-dark" onClick={e => { this.writeLogToDB() }} >Generate Log</button>
+                    
+                            <div >
+                                <li style={{
+                                    float: "left", display: "inline-block",
+                                    paddingRight: "1.8%",
+                                    marginTop: "0.01%"
+                                }}>
+                                    <button type="button" className="btn btn-outline-light text-dark" onClick={e => { this.loodPriviousFolder() }} >
+                                        <img src="https://img.icons8.com/flat_round/25/000000/arrow-left.png" />
+                                    </button>
+                                    <button type="button" className="btn btn-outline-light text-dark">
+                                        <img src="https://img.icons8.com/flat_round/25/000000/arrow-right.png" />
+                                    </button>
+                                </li>
+
+                                <label style={{ float: "left", marginTop: "1%" }}>Root
+                                {this.state.folderPath.map(data => (
+                                    <label className="path">  / {data}  </label>
+                                ))}
+                                </label>
+                            </div>
+                            <br /><br />
+                            <table class="table">
+                                <thead >
+                                    <tr>
+                                        <th>File Name</th>
+                                        <th>Type</th>
+                                        <th>Size</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            {this.state.index.map(s => (
+                                                <div>
+                                                    {s.type == "image" ?
+                                                        (<img src="https://img.icons8.com/cute-clipart/25/000000/image-file.png" />)
+                                                        :
+                                                        s.type == "folder" ?
+                                                            <img src="https://img.icons8.com/color/25/000000/folder-invoices.png" />
+                                                            :
+                                                            <img src="https://img.icons8.com/color/25/000000/txt.png" />
+                                                    }
+                                                    <button type="button" className="btn btn-outline-light text-dark" onClick={e => { this.handleClick(e, s) }}> {s.name} </button>
+
+                                                </div>
+                                            ))}
+                                            {}
+                                            <img src="https://img.icons8.com/color/25/000000/folder-invoices.png" />
+                                            <button type="button" onClick={e => { this.loodPriviousFolder() }} className="btn btn-outline-light text-dark">..</button>
+
+                                        </td>
+                                        <td>
+                                            {this.state.index.map(s => (
+                                                <div>
+                                                    <button disabled={s.type} className="btn btn-outline-light text-dark" >{s.type}</button>
+                                                </div>
+                                            ))}
+                                        </td>
+                                        <td>
+                                            {this.state.index.map(s => (
+                                                <div>
+                                                    {s.size == undefined ?
+                                                        <></>
+                                                        :
+                                                        <button disabled={s.size} className="btn btn-outline-light text-dark">{s.size}kb</button>
+                                                    }
+
+                                                </div>
+                                            ))}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Folder2;
